5e51a4cb35aeda5d3c88c6ae7d660aa4cbeb0157
Daniel Pederzini <pederzinidaniel@hotmail.com>
2026-05-09 20:14:57 -0300

    Readd existsById to card service

--- feature-manager/src/main/java/org/pdzsoftware/featuremanager/service/CardService.java
+++ feature-manager/src/main/java/org/pdzsoftware/featuremanager/service/CardService.java
@@ -17,4 +17,9 @@ public class CardService {
     public Optional<CardEntity> findById(String id) {
         return cardRepository.findById(id);
     }
+
+    @Cacheable(cacheNames = "cardsExists", key = "#a0", condition = "#a0 != null")
+    public boolean existsById(String id) {
+        return cardRepository.existsById(id);
+    }
 }
