e7112ee9d05369dda04475b43c1e23848dae03f7
Daniel Pederzini <pederzinidaniel@hotmail.com>
2026-05-09 15:27:43 -0300

    Add anti-fraud-orchestrator

---
+++ anti-fraud-orchestrator/src/main/java/org/pdzsoftware/antifraudorchestrator/AntiFraudOrchestratorApplication.java
@@ -0,0 +1,13 @@
+package org.pdzsoftware.antifraudorchestrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntiFraudOrchestratorApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(AntiFraudOrchestratorApplication.class, args);
+	}
+
+}
---
+++ anti-fraud-orchestrator/src/test/java/org/pdzsoftware/antifraudorchestrator/AntiFraudOrchestratorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pdzsoftware.antifraudorchestrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntiFraudOrchestratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
