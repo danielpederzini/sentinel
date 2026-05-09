b26f2679575f603e6992612c5e534ed48f80f612
Daniel Pederzini <pederzinidaniel@hotmail.com>
2026-05-09 16:44:51 -0300

    Add enums for CardType, DeviceType, and MerchantCategory; update Card, Merchant, and TrustedDevice classes

--- feature-manager/src/main/java/org/pdzsoftware/featuremanager/domain/model/TrustedDevice.java
+++ feature-manager/src/main/java/org/pdzsoftware/featuremanager/domain/model/TrustedDevice.java
@@ -1,9 +1,12 @@
 package org.pdzsoftware.featuremanager.domain.model;
 
+import org.pdzsoftware.featuremanager.domain.enums.DeviceType;
+
 import java.time.LocalDateTime;
 
 public abstract class TrustedDevice {
     protected String id;
     protected String name;
+    protected DeviceType deviceType;
     protected LocalDateTime creationDateTime;
 }
